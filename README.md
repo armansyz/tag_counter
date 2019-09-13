d3689bd4dd7f1d7bb4cb92fe47f098da52d3007a
lagmankz <syzdykov.arman@gmail.com>
2019-09-13 08:07:41 +0300

    Adding distribution and packaging elements

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tag Counter
+
+This package contains a program for the python beginners course.
+Application counts the tags on a given url.
+
+## Interfaces
+
+There are two interfaces:
+1. Command line interface
+2. GUI
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tag_counter_armansyz",
+    version="0.0.1",
+    author="Arman Syzdykov",
+    author_email="syzdykov.arman@gmail.com",
+    description="Package for the python beginners course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/armansyz/python_task",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+          'app_launcher': [
+              'tag_counter = tag_counter.tagcounter'
+          ]
+      },
+    test_suite='tests',
+)
